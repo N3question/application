8d7a75885f7d730d998ed4a97362ece392a8c11b
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2022-12-20 06:29:34 +0000

    [start]application-2章

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+# get 'homes/top'
+# get 'homes/top' => 'homes#top'と同じ意味。
+# 以下urlの「/homes/」を消す作業。
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
