7c7fc3bfbb30e2f55a5e52f0f8f983ae8e783e5b
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2022-12-22 20:36:58 +0000

    [add]application-10章途中

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,7 +6,9 @@ class List < ApplicationRecord
  
  validates :title,presence: true
  validates :body,presence: true
- validates :image,presence: true
+ # validates :image,presence: true  
+ # コメントアウト中。rails cの為
+ 
  # validatesで対象とする項目を指定し、
  # 入力されたデータのpresence（存在）をチェック
  # trueと記述すると、データが存在しなければならないという設定
