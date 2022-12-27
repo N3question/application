ab9404a31f607e9e7a498202928f4cfd60df4ae4
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2022-12-27 12:59:23 +0000

    [add]flush_massege

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,6 +13,13 @@ class ListsController < ApplicationController
   
   # 3. データをデータベースに保存するためのsaveメソッド実行
   # list.save
+  
+  # 以下フラッシュメッセージを定義し、詳細画面へリダイレクト
+  # flash[:notice] = "投稿が成功しました"
+  # redirect_to list_path(list.id)
+  # 書き方は特殊ですが、使い方はほとんど変数と同じ
+  # NEXT Viewファイルの記述
+  
   # validationの結果を、コントローラで検出
   # if式を用いる。
   if @list.save #以下バリデーションチェックの工程
